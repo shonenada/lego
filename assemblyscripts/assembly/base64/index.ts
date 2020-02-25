b894a384ba16f65e7c3027b2d8015a7a032f778d
shonenada <shonenada@gmail.com>
2020-02-25 21:04:49 +0800

    fix(as): remove `keyword` from `text`

--- assemblyscripts/assembly/base64/index.ts
+++ assemblyscripts/assembly/base64/index.ts
@@ -20,8 +20,10 @@ export function b64decode(len: usize): usize {
 export function _outgoing(len: usize): usize {
     const rawJson = loadStringFromMemory(len);
     const data: JSON.Obj = changetype<JSON.Obj>(JSON.parse(rawJson));
-    const text = changetype<JSON.Str>(data.get("text"))._str;
+    const rawText = changetype<JSON.Str>(data.get("text"))._str;
+    const keyword = changetype<JSON.Str>(data.get("keyword"))._str;
     const username = changetype<JSON.Str>(data.get("username"))._str;
+    const text = rawText.substring(keyword.length + 1);
     const result = ['@', username, ': ', 'Base64.encode("', text, '") = `', Base64.encode(text), '`'];
     return saveStringIntoMemory(result.join(''));
 }
